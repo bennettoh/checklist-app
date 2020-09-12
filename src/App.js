3b2e3f263871ed319b058baf12d22afd20f2e7bf
bennettoh <bennettoh.arch@gmail.com>
2020-09-12 10:56:14 -0700

    reset for tutorial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+import Table from "./components/Table";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="container">
+      <Table />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="container">
+      <App />
+    </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
